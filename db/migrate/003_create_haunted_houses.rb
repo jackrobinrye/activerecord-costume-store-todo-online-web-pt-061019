5b7dcef3a58fb7b09fa290f9ea4b1f11ba6c8e9d
Jack Atherton <jackrrye@gmail.com>
2019-08-28 20:19:52 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,6 +3,14 @@ class HauntedHouses < ActiveRecord::Migration[4.2]
 
   def change
     create_table :haunted_houses do |table|
+      table.string :name
+      table.string :location
+      table.string :theme
+      table.float :price
+      table.boolean :family_friendly
+      table.datetime :opening_date
+      table.datetime :closing_date
+      table.string :description
 
     end
   end
